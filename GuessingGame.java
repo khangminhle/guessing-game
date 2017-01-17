ef307ffcdc154cd206d9dbcd30b4c7e89292c0b4
khangminhle <15520339@gm.uit.edu.vn>
2017-01-17 13:19:05 +0700

    Add files via upload

---
+++ GuessingGame.java
@@ -0,0 +1,28 @@
+package learnjava;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessingGame {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        Scanner input = new Scanner(System.in);
+        int numberToGuess = rand.nextInt(100);
+        int numberOfTries = 0;
+        int guess;
+        boolean win = false;
+        while(!win){
+            guess = input.nextInt();
+            numberOfTries += 1;
+            if(guess == numberToGuess){
+                System.out.println("Bingo!");
+                win = true;
+            }
+            else if(guess > numberToGuess)
+                System.out.println("Too high");
+            else
+                System.out.println("Too low");
+        }
+        System.out.println("You guessed for " + numberOfTries + " tries");
+    }
+}
